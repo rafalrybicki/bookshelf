3fe054d27f966e615577777a47be201174d2197c
Rafał Rybicki <rafarybicki@gmail.com>
2022-10-04 20:10:09 +0200

    Change category model spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,32 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  subject { FactoryBot.build(:category) }
+  subject(:category) { FactoryBot.build(:category) }
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 
   describe 'associations' do
-    it { should belong_to(:owner).class_name('User') }
+    it { is_expected.to belong_to(:owner).class_name('User') }
   end
 
   context 'with valid name' do
-    it { should be_valid }
+    it { is_expected.to be_valid }
   end
 
   context 'with not valid name' do
     it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+      category.name = nil
+      expect(category).to_not be_valid
     end
 
     it 'is not valid with not unique name' do
       FactoryBot.create(:category, name: 'Horror')
-      subject.name = 'Horror'
+      category.name = 'Horror'
 
-      expect(subject).to_not be_valid
+      expect(category).to_not be_valid
     end
   end
 end
